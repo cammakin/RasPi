4ad914a6de2fd82a92e6f8c2742601981ca7beff
Cameron Makin <cammakin8@gmail.com>
2021-11-04 18:16:13 -0400

    saving work from pi

--- lcd_script.py
+++ lcd_script.py
@@ -1,14 +1,30 @@
+#!/usr/bin/python3
 import time
-# from CUSTOM_PACKAGES import LCD as lcd_module
-import cgi
+import threading
+from CUSTOM_PACKAGES import LCD as lcd_module
+from flask import Flask, request, render_template
 
-def main():
-	lcd = lcd_module.LCD()
-	formData = cgi.FieldStorage()
-	message = formData.getvalue('message')
-	# lcd.print_string("Hello World")
-	# time.sleep(1)
-	# lcd.cleanup()
+app = Flask(__name__)
+lcd = lcd_module.LCD()
+global_string = "STARTING UP"
+#lcd_thread = threading.Thread(target=lcd_driver)
+
+@app.route('/')
+def my_form():
+    return render_template("my-form.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    global_string = processed_text
+    lcd.print_string(global_string)
+    return render_template("my-form-two.html", user_data=processed_text)
+
+    
 
 if __name__ == "__main__":
-    main()
+    #lcd_thread.start()
+    lcd.print_string(global_string)
+    app.run(debug=True, host='0.0.0.0')
+    lcd.cleanup()
